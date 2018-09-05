bad195a15e31f1215cb47e8669178b1a7c1e46ec
Christoph <christoph.gnip@gmail.com>
2018-09-05 11:30:14 -0700

    :sparkles: Add Solution

--- lib/table_server_one.ex
+++ lib/table_server_one.ex
@@ -2,10 +2,10 @@ defmodule TableServerOne do
   use GenServer
 
   def init(args) do
-    # init
+    {:ok, args}
   end
 
   def handle_call(:ping, _from, []) do
-    # handle call and setup for next call
+    {:reply, {:ok}, []}
   end
 end
